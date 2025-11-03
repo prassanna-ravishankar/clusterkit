a26fec8deb24f5ad45784e1bb7ffeec2052c5102
Prass, The Nomadic Coder <atemysemicolon@gmail.com>
2025-11-03 23:37:19 +0000

    fix: use default kubeconfig path when not specified

--- cli/pkg/bootstrap/components/components.go
+++ cli/pkg/bootstrap/components/components.go
@@ -150,8 +150,18 @@ func (c *ClusterHealthChecker) Check() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
+	// Use default kubeconfig path if not specified
+	kubeconfig := c.kubeconfig
+	if kubeconfig == "" {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return fmt.Errorf("failed to get home directory: %w", err)
+		}
+		kubeconfig = filepath.Join(homeDir, ".kube", "config")
+	}
+
 	// Build Kubernetes config
-	config, err := clientcmd.BuildConfigFromFlags("", c.kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		return fmt.Errorf("failed to build kubeconfig: %w", err)
 	}
